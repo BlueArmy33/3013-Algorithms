9b17a3790c8be68640715f3edd08b7479e501a35
Andrea Lobo <117382013+BlueArmy33@users.noreply.github.com>
2025-02-03 14:45:41 -0600

    Create primer.cpp

---
+++ Mis_Notas/Codigo/primer.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+bool in_array(int *a, int index)
+  {
+    for (int i = 0; i < 
+
+int main()
+{
+  int size = 100;
+  int *nums = new int[size];
+  bool unique;
+
+  for (int i=0; i<size; i++)
+    {
+      unique = true;
+      while (unique)
+        {
+          int r = rand() % 10000;
+    
+          for (int j = 0; j < i; j++)
+            {
+            if (nums[j] == r)
+              {
+                unique = false;
+                break;
+              }
+            }
+          }
+      }
+}
